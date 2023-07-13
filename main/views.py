8502f5cae6103537413d54888177d55c210a7699
clavijo99 <bclavijogomez@gmail.com>
2023-07-12 19:03:51 -0500

    init

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request): return HttpResponse('<h1 style="width: 100%; height: 100%; display: flex; justify-content: center; align-items: center; align-content: center;" > It worked!</h1>')
+def home(request): return HttpResponse('<h1 style="width: 100%; height: 100%; display: flex; justify-content: center; align-items: center; align-content: center;" > It worked</h1>')
